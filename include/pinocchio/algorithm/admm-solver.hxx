bcc1fc90926d97c2c6fbdb4e374067ae093bc982
Louis Montaut <louismontaut@gmail.com>
2025-06-30 18:50:20 +0200

    algo/admm: fix logic of rho0

--- include/pinocchio/algorithm/admm-solver.hxx
+++ include/pinocchio/algorithm/admm-solver.hxx
@@ -310,68 +310,41 @@ namespace pinocchio
     PINOCCHIO_CHECK_ARGUMENT_SIZE(y_.size(), problem_size);
     PINOCCHIO_CHECK_ARGUMENT_SIZE(z_.size(), problem_size);
 
+    Scalar m = mu_R + mu_prox;
+    Scalar L = Scalar(-1); // not yet computed
+    Scalar rho;
+    if (rho0)
+    {
+      rho = rho0.get();
+    }
+    else
+    {
+      // Compute rho with spectral rule
+      L = computeDelassusLargestEigenvalue(G);
+      rho = ADMMSpectralUpdateRule::computeRho(L, m, rho_power);
+    }
+
     // Setup ADMM update rules:
     // Before running ADMM, we compute the largest and smallest eigenvalues of delassus in order
     // to be able to use a spectral update rule for the proximal parameter (rho)
-    // TODO should we evaluate the eigenvalues of G or Gbar ?
-    Scalar L, m, rho;
     ADMMUpdateRuleContainer admm_update_rule_container;
     switch (admm_update_rule)
     {
     case (ADMMUpdateRule::SPECTRAL): {
-      if (this->problem_size > 1)
+      if (rho0 != boost::none)
       {
-        PINOCCHIO_TRACY_ZONE_SCOPED_N("ADMMContactSolverTpl::solve - lanczos");
-        m = mu_R + mu_prox;
-        this->lanczos_decomposition.compute(G);
-        L = ::pinocchio::computeLargestEigenvalue(this->lanczos_decomposition.Ts(), 1e-8);
-#ifndef NDEBUG
-        const bool enforce_symmetry = true;
-        Eigen::MatrixXd delassus = G.matrix(enforce_symmetry);
-        Eigen::SelfAdjointEigenSolver<Eigen::MatrixXd> solver(delassus);
-        Eigen::VectorXd eigvals = solver.eigenvalues();
-        // Scalar true_m = eigvals.minCoeff();
-        Scalar true_L = eigvals.maxCoeff();
-        PINOCCHIO_UNUSED_VARIABLE(true_L);
-          //          if (true_m > 0)
-          //          {
-          //            assert(
-          //              math::fabs((true_m - m) / math::max(true_m, m)) < 0.01
-          //              && "true_m and m are too far apart.");
-          //          }
-          // assert(
-          //   math::fabs((true_L - L) / math::max(true_L, L)) < 0.01
-          //   && "true_L and L are too far apart.");
-#endif // NDEBUG
-      }
-      else
-      {
-        // TODO adapt this with Gbar
-        typedef Eigen::Matrix<Scalar, 1, 1> Vector1;
-        const Vector1 Gvec = G * Vector1::Constant(1);
-        m = L = Gvec.coeff(0);
+        // L has not yet been computed, we compute it
+        L = computeDelassusLargestEigenvalue(G);
       }
       admm_update_rule_container.spectral_rule =
         ADMMSpectralUpdateRule(ratio_primal_dual, L, m, rho_power_factor);
-      if (rho0)
-        rho = rho0.get();
-      else
-        rho = ADMMSpectralUpdateRule::computeRho(L, m, rho_power);
       break;
     }
     case (ADMMUpdateRule::LINEAR):
       admm_update_rule_container.linear_rule =
         ADMMLinearUpdateRule(ratio_primal_dual, linear_update_rule_factor);
-      if (rho0)
-        rho = rho0.get();
-      else
-        rho = this->rho; // use the rho value stored in the solver.
       break;
     case (ADMMUpdateRule::CONSTANT):
-      if (rho0)
-        rho = rho0.get();
-      else
-        rho = this->rho; // use the rho value stored in the solver.
       break;
     }
     const Scalar rho_min = 1e-6;
