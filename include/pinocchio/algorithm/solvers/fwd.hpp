1fa374ac49fc59e70a20c981fcf521576ca7cf44
Justin Carpentier <justin.carpentier@inria.fr>
2025-07-31 11:01:46 +0200

    algo: add solvers directory

---
+++ include/pinocchio/algorithm/solvers/fwd.hpp
@@ -0,0 +1,14 @@
+//
+// Copyright (c) 2025 INRIA
+//
+
+#ifndef __pinocchio_algorithm_solvers_fwd_hpp__
+#define __pinocchio_algorithm_solvers_fwd_hpp__
+
+#include "pinocchio/algorithm/fwd.hpp"
+
+namespace pinocchio
+{
+} // namespace pinocchio
+
+#endif // ifndef __pinocchio_algorithm_solvers_fwd_hpp__
