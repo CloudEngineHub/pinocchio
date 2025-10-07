ac367c165b1944bfa7aff00dcb1a2af7acb01c62
Justin Carpentier <justin.carpentier@inria.fr>
2025-10-07 16:35:29 +0200

    constraints/utils: add doc

--- include/pinocchio/algorithm/constraints/utils.hpp
+++ include/pinocchio/algorithm/constraints/utils.hpp
@@ -10,15 +10,36 @@
 namespace pinocchio
 {
 
-  template<typename ConstraintModel, class ConstraintAllocator>
-  typename internal::template std_vector_with_same_allocator<
-    std::vector<ConstraintModel, ConstraintAllocator>>::
-    template type<typename ConstraintModel::ConstraintData>
-    createData(const std::vector<ConstraintModel, ConstraintAllocator> & constraint_models)
+  /**
+   * @brief Create a vector of ConstraintData from a vector of ConstraintModel.
+   *
+   * This function iterates over a vector of constraint models and calls
+   * `createData()` on each model to generate the associated constraint data.
+   * The resulting data is collected into a vector that uses the same allocator
+   * as the input vector.
+   *
+   * @tparam ConstraintModel The type of constraint model. It must define a
+   *         nested type `ConstraintData` and have a method `ConstraintData createData() const`.
+   * @tparam ConstraintModelAllocator The allocator used for the input vector of constraint models.
+   *
+   * @param constraint_models A vector of constraint models used to generate the constraint data.
+   *
+   * @return A vector of `ConstraintModel::ConstraintData` using the same allocator
+   *         as the input vector type. Each element corresponds to the result of
+   *         `createData()` called on the respective constraint model.
+   *
+   * @note The return type is computed using `internal::std_vector_with_same_allocator`,
+   *       which ensures that the output vector has a compatible allocator with the input vector.
+   */
+  template<typename ConstraintModel, class ConstraintModelAllocator>
+    typename internal::template std_vector_with_same_allocator
+    < std::vector<ConstraintModel, ConstraintModelAllocator>::template type<
+      typename ConstraintModel::ConstraintData>
+    createData(const std::vector<ConstraintModel, ConstraintModelAllocator> & constraint_models)
   {
-    typedef typename internal::template std_vector_with_same_allocator<
-      std::vector<ConstraintModel, ConstraintAllocator>>::
-      template type<typename ConstraintModel::ConstraintData>
+    typedef typename internal::template std_vector_with_same_allocator
+      < std::vector<ConstraintModel, ConstraintModelAllocator>::template type<
+        typename ConstraintModel::ConstraintData>
         ReturnType;
 
     ReturnType constraint_datas;
