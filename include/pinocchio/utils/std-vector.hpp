48f55144aea38cdc6b3c6a6e2058de456bf9c862
Justin Carpentier <justin.carpentier@inria.fr>
2025-10-07 09:32:39 +0200

    doc: add doc for make_held_vector

--- include/pinocchio/utils/std-vector.hpp
+++ include/pinocchio/utils/std-vector.hpp
@@ -39,6 +39,32 @@ namespace pinocchio
     std::for_each(vector.begin(), vector.end(), func);
   }
 
+  /**
+   * @brief Creates a vector of holder objects that wrap the elements of a given vector.
+   *
+   * This function takes a reference to a `std::vector` of elements of type `T`
+   * and constructs a new `std::vector` containing `Holder<T>` objects,
+   * each created from the corresponding element in the input vector.
+   *
+   * Typical use case: producing a vector of `std::reference_wrapper<T>` or
+   * other holder objects for easy element access or reference semantics.
+   *
+   * @tparam T         The element type stored in the input vector.
+   * @tparam Allocator The allocator type used by the input vector.
+   * @tparam Holder    A class template that accepts a single template parameter
+   *                   (for example, `std::reference_wrapper` or a custom holder template).
+   * @param vec        Reference to the vector containing elements of type `T`.
+   *
+   * @return A new vector of type `std::vector<Holder<T>>`, where each element
+   *         wraps or references the corresponding element from the input vector.
+   *
+   * @note If `Holder` is a reference wrapper (e.g. `std::reference_wrapper`),
+   *       the returned holders will refer to the original elements in `vec`.
+   *       Make sure the lifetime of `vec` exceeds the lifetime of the returned vector
+   *       to avoid dangling references.
+   *
+   * @see std::reference_wrapper
+   */
   template<typename T, typename Allocator, template<typename H> class Holder>
   std::vector<Holder<T>> make_held_vector(std::vector<T, Allocator> & vec)
   {
@@ -46,6 +72,32 @@ namespace pinocchio
     return WrappedTVector(vec.cbegin(), vec.cend());
   }
 
+  /**
+   * @brief Creates a vector of holder objects that wrap the elements of a given const vector.
+   *
+   * This function takes a constant reference to a `std::vector` of elements of type `T`
+   * and returns a new `std::vector` containing `Holder<const T>` objects constructed
+   * from each element of the input vector.
+   *
+   * Typical use case: producing a vector of `std::reference_wrapper<const T>` or
+   * another lightweight holder type from a vector of const elements.
+   *
+   * @tparam T         The element type stored (const-qualified) in the input vector.
+   * @tparam Allocator The allocator type used by the input vector.
+   * @tparam Holder    A class template that accepts a single type parameter
+   *                   (e.g., `std::reference_wrapper` or a custom holder template).
+   * @param vec        The input vector containing elements of type `const T`.
+   *
+   * @return A new vector of type `std::vector<Holder<const T>>`, where each element
+   *         wraps the corresponding element from the input vector.
+   *
+   * @note The elements are copied or wrapped using the constructor of `Holder<const T>`
+   *       that takes a `const T&`. To avoid dangling references, ensure the lifetime
+   *       of the original elements outlives the returned holders if `Holder` is a reference
+   * wrapper.
+   *
+   * @see std::reference_wrapper
+   */
   template<typename T, typename Allocator, template<typename H> class Holder>
   std::vector<Holder<const T>> make_held_vector(const std::vector<const T, Allocator> & vec)
   {
