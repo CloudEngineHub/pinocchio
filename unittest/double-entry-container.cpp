4c7bee543ebf199135d8c89d119255935527cbc9
Justin Carpentier <justin.carpentier@inria.fr>
2025-11-03 17:50:21 +0100

    test/container: test DoubleEntryContainer with MatrixStack as holder type

--- unittest/double-entry-container.cpp
+++ unittest/double-entry-container.cpp
@@ -3,21 +3,24 @@
 //
 
 #include "pinocchio/container/double-entry-container.hpp"
+#include "pinocchio/container/matrix-stack.hpp"
 
 #include <iostream>
 #include <boost/test/unit_test.hpp>
 #include <boost/utility/binary.hpp>
 
 using namespace pinocchio;
-typedef Eigen::Matrix<double, 6, 6> Matrix6;
-typedef Eigen::aligned_allocator<Matrix6> Allocator;
-
-typedef container::DoubleEntryContainer<Matrix6, Allocator> Container;
 
 BOOST_AUTO_TEST_SUITE(BOOST_TEST_MODULE)
 
-BOOST_AUTO_TEST_CASE(test_all)
+BOOST_AUTO_TEST_CASE(test_all_std_vector)
 {
+  typedef Eigen::Matrix<double, 6, 6> Matrix6;
+  typedef Eigen::aligned_allocator<Matrix6> Allocator;
+
+  typedef std::vector<Matrix6, Allocator> Vector;
+  typedef container::DoubleEntryContainer<Vector> Container;
+
   const Eigen::Index nrows = 20, ncols = 20;
 
   Container container(nrows, ncols);
@@ -112,6 +115,39 @@ BOOST_AUTO_TEST_CASE(test_all)
   container[{4, 4}] = Matrix6::Identity();
   BOOST_CHECK(container.keys()(4, 4) == long(nrows - 1));
   BOOST_CHECK(container != copy);
+}
+
+BOOST_AUTO_TEST_CASE(test_all_matrix_stack)
+{
+  typedef Eigen::Matrix<double, 6, 6> Matrix6;
+
+  typedef MatrixStackTpl<Matrix6> Vector;
+  typedef container::DoubleEntryContainer<Vector> Container;
+
+  const Eigen::Index nrows = 20, ncols = 20;
+
+  Container container(nrows, ncols);
+
+  BOOST_CHECK(container.size() == 0);
+  BOOST_CHECK(container.rows() == nrows);
+  BOOST_CHECK(container.cols() == ncols);
+  BOOST_CHECK(container.begin() == container.end());
+
+  container.reserve(size_t(nrows));
+  BOOST_CHECK(container.values().capacity() == size_t(nrows));
+
+  // Fill diagonal with random values
+  for (Eigen::Index id = 0; id < nrows; id++)
+  {
+    const bool res = container.insert(id, id, Matrix6::Constant(double(id)));
+    BOOST_CHECK(res);
+    BOOST_CHECK(container.size() == size_t(id + 1));
+  }
+
+  for (Eigen::Index id = 0; id < nrows; id++)
+  {
+    BOOST_CHECK(container.values()[size_t(id)].isConstant(double(id), double(0)));
+  }
 }
 
 BOOST_AUTO_TEST_SUITE_END()
