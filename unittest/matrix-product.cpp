e6a385b7be96238dc0f47450186ec5dfeb716cbe
Justin Carpentier <justin.carpentier@inria.fr>
2025-12-01 15:50:30 +0100

    test/linalg: extends testing for row major types

--- unittest/matrix-product.cpp
+++ unittest/matrix-product.cpp
@@ -23,12 +23,28 @@ const int N = int(1e4);
 
 BOOST_AUTO_TEST_CASE(test_col_major)
 {
+  typedef Eigen::MatrixXd Matrix;
   const Eigen::DenseIndex rows = 10, cols = 20, inner_dim = 30;
   for (int i = 0; i < N; ++i)
   {
-    const Eigen::MatrixXd lhs = Eigen::MatrixXd::Random(rows, inner_dim),
-                          rhs = Eigen::MatrixXd::Random(inner_dim, cols);
-    Eigen::MatrixXd res = Eigen::MatrixXd::Zero(rows, cols);
+    const Matrix lhs = Matrix::Random(rows, inner_dim), rhs = Matrix::Random(inner_dim, cols);
+    Matrix res = Matrix::Zero(rows, cols);
+    const auto res_gt = (lhs * rhs).eval();
+    matrix_product(lhs, rhs, res);
+
+    BOOST_CHECK(res.isApprox(res_gt, 1e-14));
+  }
+}
+
+BOOST_AUTO_TEST_CASE(test_row_major)
+{
+  typedef PINOCCHIO_EIGEN_PLAIN_ROW_MAJOR_TYPE(Eigen::MatrixXd) Matrix;
+
+  const Eigen::DenseIndex rows = 10, cols = 20, inner_dim = 30;
+  for (int i = 0; i < N; ++i)
+  {
+    const Matrix lhs = Matrix::Random(rows, inner_dim), rhs = Matrix::Random(inner_dim, cols);
+    Matrix res = Matrix::Zero(rows, cols);
     const auto res_gt = (lhs * rhs).eval();
     matrix_product(lhs, rhs, res);
 
