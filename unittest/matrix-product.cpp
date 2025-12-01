4a5ee935904134ccb3742791b5eac3341d8ac2ca
Justin Carpentier <justin.carpentier@inria.fr>
2025-12-01 16:27:38 +0100

    test/linalg: extend testing

--- unittest/matrix-product.cpp
+++ unittest/matrix-product.cpp
@@ -21,35 +21,48 @@ const int N = int(1e3);
 const int N = int(1e4);
 #endif
 
-BOOST_AUTO_TEST_CASE(test_col_major)
+template<typename Matrix, template<typename, typename> class EigenOp>
+void test()
 {
-  typedef Eigen::MatrixXd Matrix;
+  using Scalar = typename Matrix::Scalar;
   const Eigen::DenseIndex rows = 10, cols = 20, inner_dim = 30;
+
   for (int i = 0; i < N; ++i)
   {
     const Matrix lhs = Matrix::Random(rows, inner_dim), rhs = Matrix::Random(inner_dim, cols);
-    Matrix res = Matrix::Zero(rows, cols);
-    const auto res_gt = (lhs * rhs).eval();
-    matrix_product(lhs, rhs, res);
+    Matrix res = Matrix::Random(rows, cols);
+
+    Matrix res_gt = Matrix::Zero(rows, cols);
+    typedef EigenOp<Scalar, Scalar> Op;
+
+    if constexpr (internal::is_specialization_of_v<Op, Eigen::internal::assign_op>)
+      res_gt = (lhs * rhs).eval();
+    else if constexpr (internal::is_specialization_of_v<Op, Eigen::internal::add_assign_op>)
+      res_gt = res + (lhs * rhs).eval();
+    else if constexpr (internal::is_specialization_of_v<Op, Eigen::internal::sub_assign_op>)
+      res_gt = res - (lhs * rhs).eval();
+
+    matrix_product<EigenOp>(lhs, rhs, res);
 
     BOOST_CHECK(res.isApprox(res_gt, 1e-14));
   }
+}
+
+BOOST_AUTO_TEST_CASE(test_col_major)
+{
+  typedef Eigen::MatrixXd Matrix;
+  test<Matrix, Eigen::internal::assign_op>();
+  test<Matrix, Eigen::internal::add_assign_op>();
+  test<Matrix, Eigen::internal::sub_assign_op>();
 }
 
 BOOST_AUTO_TEST_CASE(test_row_major)
 {
   typedef PINOCCHIO_EIGEN_PLAIN_ROW_MAJOR_TYPE(Eigen::MatrixXd) Matrix;
 
-  const Eigen::DenseIndex rows = 10, cols = 20, inner_dim = 30;
-  for (int i = 0; i < N; ++i)
-  {
-    const Matrix lhs = Matrix::Random(rows, inner_dim), rhs = Matrix::Random(inner_dim, cols);
-    Matrix res = Matrix::Zero(rows, cols);
-    const auto res_gt = (lhs * rhs).eval();
-    matrix_product(lhs, rhs, res);
-
-    BOOST_CHECK(res.isApprox(res_gt, 1e-14));
-  }
+  test<Matrix, Eigen::internal::assign_op>();
+  test<Matrix, Eigen::internal::add_assign_op>();
+  test<Matrix, Eigen::internal::sub_assign_op>();
 }
 
 BOOST_AUTO_TEST_SUITE_END()
