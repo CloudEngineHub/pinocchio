361ffe1aff13f9b6b6a114d22a7e5b874e35c33e
Joris Vaillant <joris.vaillant@inria.fr>
2026-02-02 13:48:18 +0100

    utils: Create public headers

---
+++ include/pinocchio/utils/static-if.hpp
@@ -0,0 +1,11 @@
+//
+// Copyright (c) 2026 INRIA
+//
+#pragma once
+
+// IWYU pragma: begin_keep
+#include "pinocchio/macros.hpp"
+#include "pinocchio/common-traits.hpp"
+// IWYU pragma: end_keep
+
+#include "pinocchio/utils/static-if.hxx" // IWYU pragma: export
