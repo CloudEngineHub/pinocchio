fba4479b18d3a543324291142b82825f45920f82
Joris Vaillant <joris.vaillant@inria.fr>
2026-02-02 14:31:53 +0100

    common: Create public headers

---
+++ include/pinocchio/common.hpp
@@ -0,0 +1,14 @@
+//
+// Copyright (c) INRIA 2026
+//
+#pragma once
+
+// IWYU pragma: begin_keep
+#include "pinocchio/traits.hpp"
+// IWYU pragma: end_keep
+
+// IWYU pragma: begin_exports
+#include "pinocchio/common/fwd.hxx"
+#include "pinocchio/common/model-entity.hxx"
+#include "pinocchio/common/data-entity.hxx"
+// IWYU pragma: end_exports
