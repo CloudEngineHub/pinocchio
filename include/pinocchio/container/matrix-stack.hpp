d06e19921f01c4161ef27c472a7597597820c1ac
Joris Vaillant <joris.vaillant@inria.fr>
2026-02-02 14:36:41 +0100

    container: Add public headers

---
+++ include/pinocchio/container/matrix-stack.hpp
@@ -0,0 +1,25 @@
+//
+// Copyright (c) INRIA 2026
+//
+#pragma once
+
+// IWYU pragma: begin_keep
+#include <cassert>
+#include <algorithm>
+#include <functional>
+#include <utility>
+#include <type_traits>
+#include <vector>
+#include <cstring>
+#include <cstddef>
+#include <new>
+
+#include <Eigen/Core>
+
+#include "pinocchio/macros.hpp"
+#include "pinocchio/eigen-common.hpp"
+// IWYU pragma: end_keep
+
+// IWYU pragma: begin_exports
+#include "pinocchio/container/matrix-stack.hxx"
+// IWYU pragma: end_exports
