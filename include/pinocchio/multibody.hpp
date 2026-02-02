bc158acd9db6b86cdd472ccf59b41817aa1e2ec6
Joris Vaillant <joris.vaillant@inria.fr>
2026-02-02 14:33:06 +0100

    multibody: Create public headers

---
+++ include/pinocchio/multibody.hpp
@@ -0,0 +1,21 @@
+//
+// Copyright (c) INRIA 2026
+//
+#pragma once
+
+// IWYU pragma: begin_keep
+#include <string>
+#include <ostream>
+
+#include <Eigen/Core>
+
+#include "pinocchio/context.hxx"
+#include "pinocchio/traits.hpp"
+
+#include "pinocchio/spatial.hpp"
+// IWYU pragma: end_keep
+
+// IWYU pragma: begin_exports
+#include "pinocchio/multibody/fwd.hxx"
+#include "pinocchio/multibody/model-item.hxx"
+// IWYU pragma: end_exports
