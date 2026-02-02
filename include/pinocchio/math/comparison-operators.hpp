75aaa2021c89feae3b1402c57414bc53347af067
Joris Vaillant <joris.vaillant@inria.fr>
2026-02-02 14:01:59 +0100

    math: Create more public headers

---
+++ include/pinocchio/math/comparison-operators.hpp
@@ -0,0 +1,11 @@
+//
+// Copyright (c) 2026 INRIA
+//
+#pragma once
+
+// IWYU pragma: begin_keep
+// IWYU pragma: end_keep
+
+// IWYU pragma: begin_exports
+#include "pinocchio/math/comparison-operators.hxx"
+// IWYU pragma: end_exports
