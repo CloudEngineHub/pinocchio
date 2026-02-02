5b617d68eeb735f9a16e3bcde1c18de0ab1800ce
Joris Vaillant <joris.vaillant@inria.fr>
2026-02-02 14:25:04 +0100

    liegroup: Create public headers

---
+++ include/pinocchio/multibody/liegroup/liegroup-variant-visitors.hpp
@@ -0,0 +1,12 @@
+//
+// Copyright (c) INRIA 2026
+//
+#pragma once
+
+// IWYU pragma: begin_keep
+#include "pinocchio/multibody/liegroup.hpp"
+// IWYU pragma: end_keep
+
+// IWYU pragma: begin_exports
+#include "pinocchio/multibody/liegroup/liegroup-variant-visitors.hxx"
+// IWYU pragma: end_exports
