6fb21bc9a4bf7214c70b9e0ab8c47daf9bcc2d1c
Joris Vaillant <joris.vaillant@inria.fr>
2026-02-03 09:51:21 +0100

    multibody/pool: Create public headers

---
+++ include/pinocchio/multibody/pool.hpp
@@ -0,0 +1,26 @@
+//
+// Copyright (c) INRIA 2026
+//
+#pragma once
+
+// IWYU pragma: begin_keep
+#include <algorithm>
+#include <iterator>
+#include <vector>
+#include <cstddef>
+
+#include <omp.h>
+
+#include "pinocchio/context.hxx"
+#include "pinocchio/macros.hpp"
+
+#include "pinocchio/utils/openmp.hpp"
+
+#include "pinocchio/multibody.hpp"
+// IWYU pragma: end_keep
+
+// IWYU pragma: begin_exports
+#include "pinocchio/multibody/pool/fwd.hxx"
+#include "pinocchio/multibody/pool/model.hxx"
+#include "pinocchio/multibody/pool/geometry.hxx"
+// IWYU pragma: end_exports
---
+++ include/pinocchio/multibody/pool/model.hpp
@@ -0,0 +1,12 @@
+//
+// Copyright (c) INRIA 2026
+//
+#pragma once
+
+// IWYU pragma: begin_keep
+#include "pinocchio/multibody/pool.hpp"
+// IWYU pragma: end_keep
+
+// IWYU pragma: begin_exports
+#include "pinocchio/multibody/pool/model.hxx"
+// IWYU pragma: end_exports
