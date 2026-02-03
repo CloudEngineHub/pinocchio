b3bf2fa278038a2673f6c621a3c3d3359e2ae130
Joris Vaillant <joris.vaillant@inria.fr>
2026-02-03 14:03:07 +0100

    serialization: Create public headers

---
+++ include/pinocchio/serialization/joints-motion.hpp
@@ -0,0 +1,12 @@
+//
+// Copyright (c) 2026 INRIA
+//
+#pragma once
+
+// IWYU pragma: begin_keep
+#include "pinocchio/serialization.hpp"
+// IWYU pragma: end_keep
+
+// IWYU pragma: begin_exports
+#include "pinocchio/serialization/joints-motion.hxx"
+// IWYU pragma: end_exports
