dc6b53b7fd33c4a958853d419cdb14aee9af9275
Joris Vaillant <joris.vaillant@inria.fr>
2026-02-03 17:02:26 +0100

    parsers/graph: Create public headers

---
+++ include/pinocchio/parsers/graph.hpp
@@ -0,0 +1,48 @@
+//
+// Copyright (c) INRIA 2026
+//
+#pragma once
+
+// IWYU pragma: begin_keep
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <stdexcept>
+#include <utility>
+
+#include <boost/optional.hpp>
+#include <boost/none.hpp>
+#include <boost/variant/variant.hpp>
+#include <boost/variant/apply_visitor.hpp>
+#include <boost/variant/recursive_wrapper.hpp>
+#include <boost/variant/static_visitor.hpp>
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/graph_selectors.hpp>
+#include <boost/graph/graph_traits.hpp>
+
+#include <Eigen/Core>
+
+#include "pinocchio/context.hxx"
+#include "pinocchio/macros.hpp"
+
+#include "pinocchio/utils/check.hpp"
+
+#include "pinocchio/spatial.hpp"
+#include "pinocchio/math.hpp"
+#include "pinocchio/multibody.hpp"
+#include "pinocchio/multibody/joint.hpp"
+
+#include "pinocchio/parsers/config.hpp"
+#include "pinocchio/parsers/meshloader-fwd.hxx"
+// IWYU pragma: end_keep
+
+// IWYU pragma: begin_exports
+#include "pinocchio/parsers/graph/fwd.hxx"
+#include "pinocchio/parsers/graph/frames.hxx"
+#include "pinocchio/parsers/graph/joints.hxx"
+#include "pinocchio/parsers/graph/geometries.hxx"
+#include "pinocchio/parsers/graph/model-graph.hxx"
+#include "pinocchio/parsers/graph/graph-visitor.hxx"
+#include "pinocchio/parsers/graph/model-graph-algo.hxx"
+#include "pinocchio/parsers/graph/model-graph-algo-geometry.hxx"
+// IWYU pragma: end_exports
