1f0601daa71fae49dc8bcda5972611f53aace8c9
Joris Vaillant <joris.vaillant@inria.fr>
2026-02-04 14:15:40 +0100

    parsers/mjcf: Create public headers

---
+++ include/pinocchio/parsers/mjcf.hpp
@@ -0,0 +1,14 @@
+//
+// Copyright (c) INRIA 2026
+//
+#pragma once
+
+// IWYU pragma: begin_keep
+// IWYU pragma: end_keep
+
+// IWYU pragma: begin_exports
+#include "pinocchio/parsers/mjcf/mjcf.hxx"
+#include "pinocchio/parsers/mjcf/mjcf-graph.hxx"
+#include "pinocchio/parsers/mjcf/model.hxx"
+#include "pinocchio/parsers/mjcf/geometry.hxx"
+// IWYU pragma: end_exports
