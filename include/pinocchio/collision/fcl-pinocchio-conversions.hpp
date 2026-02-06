07262b3890ea0d884492ad70e424a4aebc7b9218
Joris Vaillant <joris.vaillant@inria.fr>
2026-02-06 10:55:16 +0100

    iwyu: Export coal-pinocchio-conversions in fcl-pinocchio-conversions

--- include/pinocchio/collision/fcl-pinocchio-conversions.hpp
+++ include/pinocchio/collision/fcl-pinocchio-conversions.hpp
@@ -8,9 +8,11 @@
 #include "pinocchio/deprecated.hpp"
 
 #include "pinocchio/spatial.hpp"
-
-#include "pinocchio/collision/coal-pinocchio-conversions.hpp"
 // IWYU pragma: end_keep
+
+// IWYU pragma: begin_exports
+#include "pinocchio/collision/coal-pinocchio-conversions.hpp"
+// IWYU pragma: end_exports
 
 // clang-format off
 PINOCCHIO_MOVED_HEADER_PINOCCHIO4(pinocchio/collision/fcl-pinocchio-conversions.hpp, pinocchio/collision/coal-pinocchio-conversions.hpp)
