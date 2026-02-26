88c314a2ae01de30f01834a1eeeac9bb784256d5
Joris Vaillant <joris.vaillant@inria.fr>
2026-02-26 17:06:40 +0100

    constraints: Add constraint-cholesky-decl.hpp standalone header

---
+++ include/pinocchio/algorithm/constraint-cholesky-decl.hpp
@@ -0,0 +1,25 @@
+//
+// Copyright (c) INRIA 2026
+//
+
+#pragma once
+
+// IWYU pragma: begin_keep
+#include <cstddef>
+#include <vector>
+
+#include <Eigen/Core>
+
+#include "pinocchio/macros.hpp"
+#include "pinocchio/deprecated.hpp"
+#include "pinocchio/eigen-common.hpp"
+
+#include "pinocchio/container/eigen-storage.hpp"
+
+#include "pinocchio/multibody/fwd.hpp"
+// IWYU pragma: end_keep
+
+// IWYU pragma: begin_exports
+#include "pinocchio/algorithm/fwd.hpp"
+#include "pinocchio/algorithm/constraint-cholesky-decl.hxx"
+// IWYU pragma: end_exports
