c42469a81a7506b1a8be1e3cf3326c0a04e85b41
Joris Vaillant <joris.vaillant@inria.fr>
2026-02-27 14:58:45 +0100

    constraints: Port proximal public header

---
+++ include/pinocchio/algorithm/proximal.hpp
@@ -0,0 +1,18 @@
+//
+// Copyright (c) 2026 INRIA
+//
+#pragma once
+
+// IWYU pragma: begin_keep
+#include <cassert>
+
+#include <Eigen/Core>
+
+#include "pinocchio/macros.hpp"
+
+#include "pinocchio/utils/check.hpp"
+// IWYU pragma: end_keep
+
+// IWYU pragma: begin_exports
+#include "pinocchio/algorithm/proximal.hxx"
+// IWYU pragma: end_exports
